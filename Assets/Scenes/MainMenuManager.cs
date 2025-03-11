f3a5b3e82d9b719be165e1dc5aaf419e0d5bd690
BobSol02 <bobansolomou@gmail.com>
2025-03-11 15:45:44 +0200

    test

--- Assets/Scenes/MainMenuManager.cs
+++ Assets/Scenes/MainMenuManager.cs
@@ -1,10 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_ANDROID
+using UnityEngine.Android;
+#endif
+
 
 public class MainMenuManager : MonoBehaviour
 {
     public void LoadARScene()
     {
+#if UNITY_IOS
+        if(!Application.HasUserAuthorization(UserAuthorization.WebCam)){
+            Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        }
+        Debug.Log("Apple");
+#endif
+#if UNITY_ANDROID
+        if(!Permission.HasUserAuthorizedPermission(Permission.Camera)){
+        Debug.Log("Asking Permission");
+            Permission.RequestUserPermission(Permission.Camera);
+        }
+        Debug.Log("Android");
+#endif
         SceneManager.LoadScene("ARScene");
     }
 
